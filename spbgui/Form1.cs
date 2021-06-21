0efa388a9c266df15ad49ac94eb5521f6f366a87
Sayan Biswas <sayan@pokurt.me>
2021-06-21 23:59:11 +0530

    Don't display null values

--- spbgui/Form1.cs
+++ spbgui/Form1.cs
@@ -27,14 +27,26 @@ namespace spbgui
             //label2.Text = $"Private telegram ID: {data.Results.PrivateTelegramId}\n";
             label2.Text = $"Entity Type: {data.Results.EntityType}\n";
             label2.Text += $"Blacklisted: {data.Results.Attributes.IsBlacklisted}\n";
-            label2.Text += $"Blacklist Flag: {data.Results.Attributes.BlacklistFlag}\n";
-            label2.Text += $"Blacklist Reason: {data.Results.Attributes.BlacklistReason}\n";
+            if (data.Results.Attributes.BlacklistFlag != null)
+            {
+                label2.Text += $"Blacklist Flag: {data.Results.Attributes.BlacklistFlag}\n";
+            }
+
+            if (data.Results.Attributes.BlacklistReason != null)
+            {
+                label2.Text += $"Blacklist Reason: {data.Results.Attributes.BlacklistReason}\n";
+            }
+
+            if (data.Results.Attributes.OriginalPrivateId != null)
+            {
+                label2.Text += $"Original PTID (only for 0xEVADE): {data.Results.Attributes.OriginalPrivateId}\n";
+            }
+
             label2.Text += $"Official: {data.Results.Attributes.IsOfficial}\n";
             label2.Text += $"Operator: {data.Results.Attributes.IsOperator}\n";
             label2.Text += $"Whitelisted: {data.Results.Attributes.IsWhitelisted}\n";
             label2.Text += $"Potential Spammer: {data.Results.Attributes.IsPotentialSpammer}\n";
             label2.Text += $"Intellivoid Accounts verified: {data.Results.Attributes.IntellivoidAccountsVerified}\n";
-            label2.Text += $"Original PTID (only for 0xEVADE): {data.Results.Attributes.OriginalPrivateId}\n";
             label2.Text += $"Language: {data.Results.LanguagePrediction.Language}";
 
             label3.Text = $"Private telegram ID: {data.Results.PrivateTelegramId}";
@@ -62,6 +74,11 @@ namespace spbgui
         }
 
         private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
