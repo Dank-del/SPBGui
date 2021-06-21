5613f06aeda4dfa5bca7bcababd182ed5800ce40
Sayan Biswas <sayan@pokurt.me>
2021-06-21 23:58:20 +0530

    play more with assemblyinfo

--- spbgui/Properties/AssemblyInfo.cs
+++ spbgui/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SpamProtection GUI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("GUI application to search users on Intellivoid.net SpamProtection API.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Intellivoid")]
 [assembly: AssemblyProduct("SpamProtection GUI")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright ©  2021 Dank-Del, Intellivoid")]
+[assembly: AssemblyTrademark("Intellivoid")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
