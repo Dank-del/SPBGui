2b48757b9a7586d9600bb3af9f88f474b221b4b9
Sayan Biswas <sayan@pokurt.me>
2021-06-21 23:08:38 +0530

    Dont shitpost in assemblyinfo

--- spbgui/Properties/AssemblyInfo.cs
+++ spbgui/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("gay1")]
+[assembly: AssemblyTitle("SpamProtection GUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("gay1")]
+[assembly: AssemblyProduct("SpamProtection GUI")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
